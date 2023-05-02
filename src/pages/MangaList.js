d3332fb7b5dc5dc3e8493bec842d60bb4c582ebf
mami-em <maminiainaravaloarison@gmail.com>
2023-05-02 13:13:37 -0400

    layout pages loading fixed

--- src/pages/MangaList.js
+++ src/pages/MangaList.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import HomeLoading from "../components/HomeLoading";
 
 
 export default function MangaList() {
     const [loading, setLoading] = useState(true);
 
     if (loading) return <HomeLoading />
-    
+
     return (
         <div>
             This is the manga list page
--- src/pages/Request.js
+++ src/pages/Request.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import HomeLoading from "../components/HomeLoading";
 
 
 export default function Request() {
